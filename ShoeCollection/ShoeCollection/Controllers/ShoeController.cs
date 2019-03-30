f0231722d296c16427dd8ec8836128c3c94f3ed9
Michael Manville <monstro33@yahoo.com>
2019-03-30 02:58:04 -0400

    Shoe Context, Shoe Model Complete

---
+++ ShoeCollection/ShoeCollection/Controllers/ShoeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoeCollection.Models;
+using ShoeCollection.Repositories;
+
+namespace ShoeCollection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoeController : ControllerBase
+    {
+        public ShoeBrandRepository shoesRepo;
+
+        public ShoeController(ShoeBrandRepository shoesRepo)
+        {
+            this.shoesRepo = shoesRepo;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ShoeBrand>> Get()
+        {
+            return shoesRepo.GetAll(); ;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> Post([FromBody] ShoeBrand shoeBrands)
+        {
+            shoesRepo.Create(ShoeBrand);
+            return true;
+        }
+    }
+}
---
+++ ShoeCollection/ShoeCollection/Repositories/ShoeBrandRepository.cs
@@ -0,0 +1,34 @@
+﻿using ShoeCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeCollection.Repositories
+{
+    public class ShoeBrandRepository
+    {
+        private ShoeContext db;
+
+        public ShoeBrandRepository(ShoeContext db)
+        {
+            this.db = db;
+        }
+
+        public ShoeBrand GetById(int id)
+        {
+            return db.Shoes.Single(shoeBrand => shoeBrand.ShoeBrandId == id);
+        }
+
+        public void Create(ShoeBrand newShoeBrand)
+        {
+            db.shoeBrands.Add(newShoeBrand);
+            db.SaveChanges();
+        }
+
+        public List<ShoeBrand> GetAll()
+        {
+            return db.ShoeBrands.ToList();
+        }
+    }
+}

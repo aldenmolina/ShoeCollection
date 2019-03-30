e9bcf61886fde083506cb78f0b846a71e0232457
Alden <aj.molina092@gmail.com>
2019-03-30 13:00:20 -0400

    Finished input text component

--- Frontend/src/InputText.js
+++ Frontend/src/InputText.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 
 class InputText extends Component {
-    onSetTypeName = e => {
-        this.props.setTypeName(e.target.value);
-    };
-    onSetBrandName = e => {
-        this.props.setBrandName(e.target.value);
-    };
-    onSetBrandImgPath = e => {
-        this.props.setBrandImgPath(e.target.value);
-    };
-    onSetBrandInfo = e => {
-        this.props.setBrandInfo(e.target.value);
-    };
-    onSetTypeImgPath = e => {
-        this.props.setTypeImagePath(e.target.value);
-    };
-    onSetTypeInfo = e => {
-        this.props.setTypeInfo(e.target.value);
-    };
+  onSetTypeName = e => {
+    this.props.setTypeName(e.target.value);
+  };
+  onSetBrandName = e => {
+    this.props.setBrandName(e.target.value);
+  };
+  onSetBrandImgPath = e => {
+    this.props.setBrandImgPath(e.target.value);
+  };
+  onSetBrandInfo = e => {
+    this.props.setBrandInfo(e.target.value);
+  };
+  onSetTypeImgPath = e => {
+    this.props.setTypeImagePath(e.target.value);
+  };
+  onSetTypeInfo = e => {
+    this.props.setTypeInfo(e.target.value);
+  };
 
   render() {
     const {
@@ -31,12 +31,34 @@ class InputText extends Component {
     } = this.props;
     return (
       <div>
-        <input type="text" value={shoeTypeName} onChange={}  />
-        <input type="text" value={shoeBrandName} onChange={}  />
-        <input type="text" value={brandImgPath} onChange={}  />
-        <input type="text" value={shoeBrandInfo} onChange={}  />
-        <input type="text" value={typeImagePath} onChange={}  />
-        <input type="text" value={shoeTypeInfo} onChange={}  />
+        Shoe Type:{" "}
+        <input type="text" value={shoeTypeName} onChange={this.onSetTypeName} />
+        Shoe Brand:{" "}
+        <input
+          type="text"
+          value={shoeBrandName}
+          onChange={this.onSetBrandName}
+        />
+        Brand Image URL:{" "}
+        <input
+          type="text"
+          value={brandImgPath}
+          onChange={this.onSetBrandImgPath}
+        />
+        Brand Info:{" "}
+        <input
+          type="text"
+          value={shoeBrandInfo}
+          onChange={this.onSetBrandInfo}
+        />
+        Shoe Type Image URL:{" "}
+        <input
+          type="text"
+          value={typeImagePath}
+          onChange={this.typeImagePath}
+        />
+        Shoe Type Info:{" "}
+        <input type="text" value={shoeTypeInfo} onChange={this.onSetTypeInfo} />
       </div>
     );
   }

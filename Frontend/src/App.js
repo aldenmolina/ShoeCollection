05f20770d9b97243dcad6bd56a295e3e7ff859c3
Alden <aj.molina092@gmail.com>
2019-04-01 00:16:09 -0400

    Added new shoe inputs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,15 +5,13 @@ import Shoes from "./Shoes";
 import Navigation from "./Navigation";
 import Hero from "./Hero";
 
-
-
 class App extends Component {
   constructor() {
     super();
-    this.state = {   
-      shoeTypeName: [],
-      shoeName: [],   
-      shoeDescription: "",
+    this.state = {
+      shoeName: "",
+      imagePath: "",
+      description: "",
       shoes: []
     };
   }
@@ -24,39 +22,105 @@ class App extends Component {
       .then(json => this.setState({ shoes: json }));
   }
 
-  setTypeName = newTypeName => {
-    this.setState({ shoeTypeName: newTypeName });
-  };
-  setName = newName => {
-    this.setState({ shoeName: newName });
-  };
-  setImage = newImage => {
-    this.setState({ImagePath: newImage});
-  }
-  setDescription = newDescription => {
-    this.setState({ shoeDescription: newDescription });
+  addNewShoe = shoeTypeId => {
+    const addedShoeType = {
+      shoeTypeId: shoeTypeId,
+      shoeName: this.state.shoeName,
+      imagePath: this.state.imagePath,
+      description: this.state.description
+    };
+    fetch("https://localhost:44382/api/shoetype", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(addedShoeType)
+    }).then(res => {
+      if (res.ok) {
+        const shoeTypeIndex = shoeTypeId - 1;
+        const newShoes = [
+          ...this.state.shoeType[shoeTypeIndex].shoeBrands,
+          addedShoeType
+        ];
+        const updateShoeType = this.state.shoeType[shoeTypeIndex];
+        updateShoeType.shoes = newShoes;
+        const newShoeType = this.state.shoeType;
+        newShoeType[shoeTypeIndex] = updateShoeType;
+        this.setState({ shoeType: newShoeType });
+      }
+    });
   };
 
-  setType = newType => {
-    this.setState({ shoeType: newType });
+  setShoeName = newName => {
+    this.setState({ shoeName: newName });
   };
+
+  setImagePath = newPath => {
+    this.setState({ imagePath: newPath });
+  };
+
+  setDescription = newText => {
+    this.setState({ description: newText });
+  };
+
+  // updateShoeType = (newShoeId, newShoeTypeId, newImagePath, newDescription) => {
+  //   const newShoe = {
+  //     shoeTypeName: this.state.shoeTypeName,
+  //     shoeId: newShoeId,
+  //     shoeTypeId: newShoeTypeId,
+  //     ImagePath: newImagePath,
+  //     Description: newDescription
+  //   };
+  //   fetch("https://localhost:44382/api/shoe/" + newShoeId, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(newShoe)
+  //   }).then(res => {
+  //     if (res.ok) {
+  //       const shoeTypeIndex = newShoeTypeId - 1;
+  //       const updateShoe = [...this.state.shoeType[shoeTypeIndex].shoeBrands];
+  //       let previousShoe = 0;
+  //       updateShoe.forEach((shoe, index) => {
+  //         if (shoe.shoeId === newShoeId) {
+  //           previousShoe = index;
+  //         }
+  //       }),
+  //         (updateShoe[previousShoe] = newShoe);
+  //       const updateShoeType = this.state.shoeType[shoeTypeIndex];
+  //       updateShoeType.shoeBrands = updateShoe;
+  //       const newShoeType = this.state.shoeType;
+  //       newShoeType[shoeTypeIndex] = updateShoeType;
+  //       this.setState({ shoeType: newShoeType });
+  //     }
+  //   });
+  // };
 
   render() {
     const shoeList = this.state.shoes.map(item => (
-      <Shoes shoeBrands={item.shoeBrands} shoeTypeName={item.shoeTypeName} />
+      <Shoes
+        shoeTypeId={item.shoeTypeId}
+        shoeTypeName={item.shoeTypeName}
+        shoeBrands={item.shoeBrands}
+        imagePath={this.imagePath}
+        setShoeName={this.setShoeName}
+        setImagePath={this.setImagePath}
+        setDescription={this.setDescription}
+        shoeNameText={this.shoeName}
+        addNewShoe={this.addNewShoe}
+      />
     ));
     return (
-        <div className = "main-container">
-            <header className = "header">
-              <Navigation />
-            </header>
-            <div className = "main">
-              <Hero />
-            </div> 
-            <div className = "main-2">
-              {shoeList}
-            </div>                     
-          
+      <div className="main-container">
+        <header className="header">
+          <Navigation />
+        </header>
+        <div className="main">
+          <Hero />
+        </div>
+        <div className="main-2">{shoeList}</div>
+
         {/* // <InputText
         //   shoeTypeName={this.state.shoeTypeName}
         //   shoeBrandName={this.state.shoeBrandName}
@@ -65,8 +129,7 @@ class App extends Component {
         //   setName={this.setName}
         //   setDescription={this.setDescription}
         // /> */}
-
-        </div>
+      </div>
     );
   }
 }
--- Frontend/src/Shoes.js
+++ Frontend/src/Shoes.js
@@ -2,20 +2,67 @@ import React, { Component } from "react";
 import ShoeItem from "./ShoeItem";
 
 class Shoes extends Component {
+  onShoeNameChange = e => {
+    this.props.setShoeName(e.target.value);
+  };
+  onImageChange = e => {
+    this.props.setImagePath(e.target.value);
+  };
+  onDescriptionChange = e => {
+    this.props.setDescription(e.target.value);
+  };
   render() {
-    const { shoeBrands, shoeTypeName } = this.props;
+    const {
+      shoeTypeName,
+      shoeBrands,
+      shoeNameText,
+      imagePath,
+      description
+    } = this.props;
     const shoeItems = shoeBrands.map(item => (
-      <ShoeItem shoeName={item.shoeName} imagePath={item.imagePath} description={item.description} />
+      <ShoeItem
+        shoeId={item.shoeId}
+        shoeTypeId={item.shoeTypeId}
+        shoeName={item.shoeName}
+        imagePath={item.imagePath}
+        description={item.description}
+        shoeNameText={shoeNameText}
+      />
     ));
     return (
       <body>
         <div class="card">
-         <h2>{shoeTypeName}</h2> 
-        {shoeItems}
-      </div>
+          <h2>{shoeTypeName}</h2>
+
+          <div className={`add-form${this.props.shoeTypeId}`}>
+            Shoe Name:{" "}
+            <input
+              className={`add-name${this.props.shoeTypeId}`}
+              type="text"
+              value={shoeNameText}
+              onChange={this.onShoeNameChange}
+            />
+            Image URL:{" "}
+            <input
+              className={`add-form${this.props.shoeTypeId}`}
+              type="text"
+              value={imagePath}
+              onChange={this.onImageChange}
+            />
+            Description:{" "}
+            <input
+              className={`add-form${this.props.shoeTypeId}`}
+              type="text"
+              value={description}
+              onChange={this.onDescriptionChange}
+            />
+            <button>Add Shoes</button>
+          </div>
+          <div>{shoeItems}</div>
+        </div>
       </body>
-        );
-      }
-    }
+    );
+  }
+}
 
 export default Shoes;
